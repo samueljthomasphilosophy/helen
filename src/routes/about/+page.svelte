7b109816b21b8b264ac68eaf026adf887629a85a
samueljthomasphilosophy <samthethomas@gmail.com>
2026-08-16 20:55:40 -0700

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -24,20 +24,20 @@
                 >Pima Community College</a>.
                 </p> <p class="text-xl py-1">
                 My main areas of study are in political philosophy and 19th century philosophy. I additionally have areas of concentration in environmental ethics, critical theory, and philosophy of law.
-
+                </p> <p class="text-xl py-1"></p>           
                 My dissertation concerns what to do about democratic backsliding: that is, the process whereby a democratic society becomes authoritarian. To answer this, one must first answer the question of what the value of democracy is, as well as the causes of backsliding. To that end, the dissertation will decidedly be a monograph since the topic deserves careful and elongated exposition.
-                
+                </p> <p class="text-xl py-1"></p>
                 In 19th century philosophy, I am mainly interested in the speculative and historical materialist methodologies of Hegel and Marx, respectively, and how such methods of philosophical analysis motivate these thinkers’ substantive political views. Of particular interest here is how later thinkers (e.g., the so-called Frankfurt School) use such methods of philosophical analysis to inform their critiques of everyday life. 
-
+                </p> <p class="text-xl py-1"></p>
                 In environmental ethics, I am concerned about how economic arrangements affect public environmental goods. The (mis)management of the Colorado River is one area of import for me as I am a lifelong Arizonan. I am also interested in debates around to what extent we should grant rights or socialize the natural world to deal with the effects of climate change.
-
+                </p> <p class="text-xl py-1"></p>
                 My MA thesis, gained from Arizona State University in 2021, concerned legal epistemology, specifically how judges and juries should evaluate different types of evidence. This thesis covered topics in philosophy of law, epistemology, and philosophy of language to give an account of individualized evidence as qualitatively different from bare statistical evidence. However, as the previous paragraphs indicate, my main areas of study have shifted since arriving at the University of Arizona in Fall 2022.
-
+                </p> <p class="text-xl py-1"></p>
                 Aside from my academic commitments, I am part of the Editorial Board for the
                 <a class="text-blue-700 hover:underline" href="https://ctwgwebsite.github.io"
-                >Critical Theory Working Group</a>.
+                >Critical Theory Working Group</a>,
                 </p> <p class="text-xl py-1"> and a <a class="text-blue-700 hover:underline" href="https://burialmask13.bandcamp.com/"
-                >musician of sorts</a>
+                >musician of sorts</a>.
             </p>
         </div>
     </div>
